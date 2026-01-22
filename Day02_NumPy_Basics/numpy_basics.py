e5f3c533215824a5e4f5a2f2d139a5cb982d1f27
NAVEEN  M <mnaveen1702@gmail.com>
2026-01-22 21:26:44 +0530

    Add NumPy basic programs

---
+++ Day02_NumPy_Basics/numpy_basics.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# 1D array
+arr1 = np.array([1, 2, 3, 4])
+print(arr1)
+
+# 2D array
+arr2 = np.array([[1, 2], [3, 4]])
+print(arr2)
+
+# Basic operations
+print(arr1 + 5)
+print(arr1 * 2)
